adb52b072db135b4f72a7a41d58f764dae29fdb7
Kelly Freet <kelly.freet13@yahoo.com>
2017-10-05 20:53:33 -0700

    added background and text colors

--- app/src/main/java/com/example/kelly/welcome_app/MainActivity.java
+++ app/src/main/java/com/example/kelly/welcome_app/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.kelly.welcome_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// 'best practice' is to use
+// - density-independent pixels for the dimensions (dp or dpi) of GUI components
+// - scale independent pixels for font sizes (sp)
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
